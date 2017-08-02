d11439ee84c10aa7a23188e49c5c7a5ca24c93e3
George Kontsevich <georgek@gmx.us>
2017-08-02 14:46:57 +0800

    Update: the generated files based on the preceeding commits

--- toolchain.cmake
+++ toolchain.cmake
@@ -5,7 +5,8 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
-# tells CMake not to try to link executables during its checks
+# tells CMake not to try to link executables during its interal checks
+# things are not going to link properly without a linker script
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
